87839bc46c698ff26c7f8825c13882738fbad5b8
Dhruti PAtel <pateldhruti1651998@gmail.com>
2025-05-20 17:57:38 +0530

    Added basic sequelize and cli files

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const app = express();
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Listening at port : ${ServerConfig.PORT}`);
-    LoggerConfig.info("Server started successfully",{});
+    // LoggerConfig.info("Server started successfully",{});
 })
