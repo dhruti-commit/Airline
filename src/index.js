0483a35864edc0d66e551820f321fd9bb873839d
Dhruti PAtel <pateldhruti1651998@gmail.com>
2025-05-20 11:59:57 +0530

    Server Started

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const {ServerConfig} =  require('./config');
+
+const app = express();
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Listening at port : ${ServerConfig.PORT}`);
+})
